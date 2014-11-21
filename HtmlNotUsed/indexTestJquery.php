8820c2496bdbd5e06c43dc7751956a1c6b675a8b
cdopfel <cdopfel@gmail.com>
2014-11-21 15:34:29 -0500

    Just adding all from local

---
+++ HtmlNotUsed/indexTestJquery.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>test jquery</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+    </head>
+    <body>
+        <script type = "text/javascript" src = "scripts/jquery-1.9.1.js"></script>
+        <p onclick = "alert('works');" this is a paragraph</p>
+        <div>TODO write content</div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <script type = "text/javascript" src = "scripts/jquery-1.9.1.js"></script>
+        <script type = "text/javascript" src = "hide.js"></script>
+    </head>
+    <body> 
+        <div id ="message"> welcome</div>
+    <p onclick = "alert('javascript works');">click here to verify javascript works</p>
+     <p onclick = "$(this).hide();">click here to verify jquery works; sentence will hide()</p>
+        <div>TODO write content</div>
+        <p id = "paragraph"> this is a test paragraph to hide with hide.js</p>
+    </body>
+</html>
